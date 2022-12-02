ad90603ddf36fe066a0c6ba6edd900dc46c4c0d2
Vinay <navik.vinay12@gmail.com>
2022-12-02 13:05:03 +0530

    UC2-counting occurences of words in a paragraph

--- Program.cs
+++ Program.cs
@@ -5,9 +5,7 @@
         public static void CountNumbOfOccurence(string paragraph)
         {
             MyMapNode<string, int> hashTabe = new MyMapNode<string, int>(6);
-
             string[] words = paragraph.Split(' ');
-
             foreach (string word in words)
             {
                 if (hashTabe.Exists(word.ToLower()))
@@ -22,7 +20,8 @@
         {
             Console.WriteLine("Welcome to the Demo Program on HashTables in DSA .\n");
             Console.WriteLine("Please sslect any one from below given options :\n" +
-                "1 : Checking frequency of occurence for a single line statement (UC1).\n");
+                "1 : Checking frequency of occurence of words in a single line statement (UC1).\n" +
+                "2 : Checking frequency of occurence of words in a paragraph (UC2).\n");
             int option = Convert.ToInt32((string) Console.ReadLine());
             switch(option)
             {
@@ -30,8 +29,18 @@
                     string paragraph = "To be or not to be";//UC1 (Counting frequency of occurrence of words in a sentence .
                     CountNumbOfOccurence(paragraph);
                     break;
+                case 2:                     //UC2 (Counting frequency of occurrence of words in a paragraph .
+                    string paragraph1 = "Paranoids are not paranoid " +
+                        "because they are paranoid but " +
+                        "because they keep putting themselves " +
+                        "deliberately into paranoid avoidable " +
+                        "situations";
+                    CountNumbOfOccurence(paragraph1);
+                    break;
+                default:
+                    Console.WriteLine("Invalid option selected ! Please try again .");
+                    break;
             }
-
         }
     }
 }
